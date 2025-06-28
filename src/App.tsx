a8ec837f0de2d2af0db9b93c4a06b9944301ac7c
Arthur Felipe <arthur_feelipe@hotmail.com>
2025-06-28 20:44:04 -0300

    Criacao do Heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from "./components/Heading";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading>Ola mundo 2</Heading>
+      <Heading>
+        Ola mundo 2
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <h1>Olá mundo!</h1>
     </>
   );
